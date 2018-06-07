39c565ed70b88bd52a7d2cd972800e8f8cd961f8
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-06 17:08:41 -0700

    scaffolding complete

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+  render() {
+
+    return (
+      <main>
+        <h1>Budget Tracker</h1>
+      </main>
+    );
+  }
+}
