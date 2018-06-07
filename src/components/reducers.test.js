e7c3c09f15f7b34c01b88c6265990ec95dd4b9b9
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-07 09:59:27 -0700

    pass update test

--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -1,17 +1,24 @@
 import shortid from 'shortid';
 
 const CATEGORIES_LOAD = 'CATEGORIES_LOAD';
+const CATEGORY_ADD = 'CATEGORY_ADD';
+const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
+const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
+
+
 
 function categories(state = [], { type, payload }) {
   switch (type) {
     case CATEGORIES_LOAD:
       return payload;
+    case CATEGORY_ADD:
+      return [...state, payload];
+    case CATEGORY_UPDATE:
+      return state.map(category => category.id === payload.id ? payload : category);
     default:
       return state;
   }
 }
-
-
 
 
 it('has a default value of empty array', () => {
@@ -26,8 +33,26 @@ const food = {
   budget: 100
 };
 
+
 it('loads categories', () => {
   const state = categories([], { type: CATEGORIES_LOAD, payload: [food] });
   expect(state).toEqual([food]);
 
+});
+
+it('adds a category', () => {
+  const prevState = [];
+  const state = categories(prevState, { type: CATEGORY_ADD, payload: food });
+  expect(state).toEqual([food]);
+});
+
+it('updates a category', () => {
+  const state = categories(
+    [{ id: 1, timestamp: '2018-06-07', name: 'entertainment', budget: 50 }],
+    {
+      type: CATEGORY_UPDATE,
+      payload: { id: 1, timestamp: '2018-06-07', name: 'entertainment', budget: 200 }
+    }
+  );
+  expect(state).toEqual([{ id: 1, timestamp: '2018-06-07', name: 'entertainment', budget: 200 }]);
 });
